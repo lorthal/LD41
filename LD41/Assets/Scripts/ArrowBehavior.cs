afa9e95bd8b53798bcdb82de0ebbd187d1ce2267
Grzegorian <grzegorzpiasta@gmail.com>
2018-04-21 19:20:57 +0200

    strzalka nad glowa

--- LD41/Assets/Scripts/ArrowBehavior.cs
+++ LD41/Assets/Scripts/ArrowBehavior.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class ArrowBehavior : MonoBehaviour {
     public GameObject Player;
     public GameObject Target;
+    public float offsetY;
     Transform tP;
     Transform tT;
-
     void Start()
     {
         tP = Player.transform;
         tT = Target.transform;
     }
-
+    Quaternion rot = Quaternion.Euler(90,0,0);
 
     void Update()
     {
-        transform.position = new Vector3(tP.position.x, tP.position.y, tP.position.z);
+        transform.position = new Vector3(tP.position.x, 1.8f + offsetY, tP.position.z);
 
-        //transform.rotation = new Quaternion();
+        transform.rotation = rot;
     }
 }
