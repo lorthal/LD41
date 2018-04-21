afcc9d64684fd5b677fd17ef6ad7df545380b367
Bartosz Kluchciński <bkluchcinski@gmail.com>
2018-04-21 12:57:14 +0200

    test scene

---
+++ LD41/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
