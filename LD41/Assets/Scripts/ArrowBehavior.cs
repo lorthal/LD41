269b57cabaea6d95aef4805d469a79ca2af10664
Grzegorian <grzegorzpiasta@gmail.com>
2018-04-21 20:26:31 +0200

    dzialająca strzałka

--- LD41/Assets/Scripts/ArrowBehavior.cs
+++ LD41/Assets/Scripts/ArrowBehavior.cs
@@ -1,24 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ArrowBehavior : MonoBehaviour {
+
     public GameObject Player;
     public GameObject Target;
     public float offsetY;
-    Transform tP;
+
+    Transform tH;
     Transform tT;
+
     void Start()
     {
-        tP = Player.transform;
-        tT = Target.transform;
     }
-    Quaternion rot = Quaternion.Euler(90,0,0);
 
     void Update()
     {
-        transform.position = new Vector3(tP.position.x, 1.8f + offsetY, tP.position.z);
+        tH = Player.transform;
+        tT = Target.transform;
 
-        transform.rotation = rot;
+        transform.position = new Vector3( tH.position.x , 1.8f + offsetY , tH.position.z );
+        transform.rotation = Quaternion.LookRotation( transform.position - tT.position );
+
     }
 }
