7a3862bcd1a461adc4f3891527172d6cdf766594
flurrih <ghost744@gmail.com>
2018-04-21 16:20:29 +0200

    Add client

---
+++ LD41/Assets/Networking/Packets/Headers.cs
@@ -0,0 +1,4 @@
+﻿enum Header : ushort
+{
+    Init
+}
---
+++ LD41/Assets/Networking/Packets/PacketReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+public class PacketReader : BinaryReader
+{
+    private BinaryFormatter _bf;
+    public PacketReader(byte[] data)
+        : base(new MemoryStream(data))
+    {
+        _bf = new BinaryFormatter();
+    }
+}
