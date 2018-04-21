03978c4a3fa6f607f1de29f71a904676f781c913
Grzegorian <grzegorzpiasta@gmail.com>
2018-04-22 01:49:53 +0200

    Revert "Merge branch 'master' of https://github.com/lorthal/LD41"
    This reverts commit 26c9bbb99e241ab728ab31db0d94e4dd35978b34, reversing
    changes made to 20cd5e74ee1997b36acba66a0a5fb6d39945d5d4.

--- LD41/Assets/Scripts/ArrowBehavior.cs
+++ LD41/Assets/Scripts/ArrowBehavior.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ArrowBehavior : MonoBehaviour {
 
+    public GameObject Player;
     public GameObject Target;
+    public float offsetY;
+
+    Transform tH;
+    Transform tT;
+
+    void Start()
+    {
+    }
 
     void Update()
     {
-        transform.rotation = Quaternion.LookRotation( transform.position - Target.transform.position );
+        tH = Player.transform;
+        tT = Target.transform;
+
+        transform.position = new Vector3( tH.position.x , 1.8f + offsetY , tH.position.z );
+        transform.rotation = Quaternion.LookRotation( transform.position - tT.position );
+
     }
 }
--- LD41/Assets/Scripts/Weapon.cs
+++ LD41/Assets/Scripts/Weapon.cs
@@ -4,82 +4,25 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [Header("Look rotation")]
-    public Camera playerCamera;
     public Transform rifleFront;
-    public Transform lookAtTarget;
-    public string enemyPlayerLayer;
-
-    [Header("Shooting")]
-    public GameObject bulletPrefab;
-
-    public int maxAmmo = 5;
-
-    public int ammo = 1;
-    public float shootCooldown;
-    public float bulletSpeed;
-
-    [Header("Controlls")] public string fireButton;
-
-    private LayerMask layerMask;
-
-    private float timer;
-
-    void Start()
-    {
-
-        layerMask = LayerMask.GetMask("Enviro", "Ball", enemyPlayerLayer);
-    }
+    public float rotationSpeed = 100;
 
     void Update()
     {
-        Ray cameraRay = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray cameraRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
         RaycastHit hit;
 
-        if (Physics.Raycast(cameraRay, out hit, layerMask))
+        if (Physics.Raycast(cameraRay, out hit, 100f))
         {
             Ray rifleRay = new Ray(rifleFront.position, (hit.point - rifleFront.position));
 
-            lookAtTarget.position = hit.point;
-            transform.LookAt(lookAtTarget.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(rifleRay.direction, Vector3.up), Time.deltaTime * rotationSpeed);
 
-            if (Input.GetButtonDown(fireButton))
+            if (Input.GetMouseButton(0))
             {
-                if (CanShoot())
-                {
-                    Shoot();
-                }
                 Debug.DrawRay(rifleRay.origin, rifleRay.direction * 100, Color.red);
             }
         }
-        timer -= Time.deltaTime;
-
-    }
-
-    private bool CanShoot()
-    {
-        return timer <= 0 && ammo > 0;
-    }
-
-    void Shoot()
-    {
-        GameObject rocket = Instantiate(bulletPrefab, rifleFront.position, Quaternion.LookRotation(rifleFront.forward));
-        ConstantForce force = rocket.AddComponent<ConstantForce>();
-
-        force.force = -rifleFront.forward * bulletSpeed;
-
-        ammo--;
-        timer = shootCooldown;
-    }
-
-    public bool CanPickupAmmo()
-    {
-        return ammo < maxAmmo;
-    }
-
-    public void PickupAmmo(int amount)
-    {
-        ammo += amount;
     }
 }
