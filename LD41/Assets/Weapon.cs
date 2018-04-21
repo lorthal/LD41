53a677023f1f62837d9cc6ee1ca540b7f2e01516
Bartosz Kluchciński <bkluchcinski@gmail.com>
2018-04-21 20:51:10 +0200

    bazooka IK

---
+++ LD41/Assets/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform rifleFront;
+    public float rotationSpeed = 100;
+
+    void Update()
+    {
+        Ray cameraRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(cameraRay, out hit, 100f))
+        {
+            Ray rifleRay = new Ray(rifleFront.position, (hit.point - rifleFront.position));
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(rifleRay.direction, Vector3.up), Time.deltaTime * rotationSpeed);
+
+            if (Input.GetMouseButton(0))
+            {
+                Debug.DrawRay(rifleRay.origin, rifleRay.direction * 100, Color.red);
+            }
+        }
+    }
+}
