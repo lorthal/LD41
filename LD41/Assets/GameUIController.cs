5cd429386036fcce673e26536de71a13dad388a0
flurrih <ghost744@gmail.com>
2018-04-22 14:28:53 +0200

    mroe ui

--- LD41/Assets/GameUIController.cs
+++ LD41/Assets/GameUIController.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class GameUIController : MonoBehaviour {
     
     public Text timeLeftText;
+    public Text timeToStartText;
+    public Text player1Score;
+    public Text player2Score;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,11 +18,32 @@ public class GameUIController : MonoBehaviour {
 	void Update () {
         //timeLeftText.text = string.Format((Mathf.Round((MatchController.Instance.TimeLeft / 60f)).ToString()));
 
+        if(MatchController.Instance.TimeLeft > 0)
+        {
+            int minutes = Mathf.FloorToInt(MatchController.Instance.TimeLeft / 60);
+            int seconds = Mathf.FloorToInt(MatchController.Instance.TimeLeft % 60);
 
-        int minutes = Mathf.FloorToInt(MatchController.Instance.TimeLeft / 60);
-        int seconds = Mathf.FloorToInt(MatchController.Instance.TimeLeft % 60);
+            timeLeftText.text = string.Format("{0:#0}:{1:00}",minutes, seconds);
+        }
+        else
+        {
+            timeLeftText.text = "00:00";
+        }
 
-        timeLeftText.text = string.Format("{0:#0}:{1:00}",minutes, seconds);
+
+        timeToStartText.text = Mathf.FloorToInt(MatchController.Instance.startTimer).ToString();
+        if(MatchController.Instance.startTimer < 0)
+        {
+            timeToStartText.enabled = false;
+        }
+        else
+        {
+            timeToStartText.enabled = true;
+
+        }
+
+        player1Score.text = MatchController.Instance.Player1Score.ToString();
+        player2Score.text = MatchController.Instance.Player2Score.ToString();
 
     }
 }
