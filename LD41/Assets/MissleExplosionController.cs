a3390c5ed81aa07bda2ad3e779d4563dbc051a7c
flurrih <ghost744@gmail.com>
2018-04-22 09:08:30 +0200

    added explosion controller

---
+++ LD41/Assets/MissleExplosionController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleExplosionController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("q"))
+        {
+            Explode();
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider != null)
+        {
+            Explode();
+        }
+    }
+
+
+    void Explode()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, 10f);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(1000f, explosionPos, 10f, 3.0F);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
