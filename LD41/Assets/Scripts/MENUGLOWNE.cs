2076b2aa9618b130cc316af4cb5b0188a484b747
Grzegorian <grzegorzpiasta@gmail.com>
2018-04-22 12:05:04 +0200

    menu glowne

---
+++ LD41/Assets/Scripts/MENUGLOWNE.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENUGLOWNE : MonoBehaviour {
+    
+    public void ChangeScene(string sceneName)
+    {
+        //Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
